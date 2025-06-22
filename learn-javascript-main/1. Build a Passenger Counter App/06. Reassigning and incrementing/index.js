33e3643254b3a2f609a7a0b2cdda217f135276f0
Krishna Gehlot <pkrishna.gehlot@gmail.com>
2025-06-23 03:44:48 +0800

    Reassigning variables and inxrementing/decrementing.

--- learn-javascript-main/1. Build a Passenger Counter App/06. Reassigning and incrementing/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/06. Reassigning and incrementing/index.js
@@ -6,5 +6,14 @@ console.log(count)
 
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
 // Decrease it down to 25, and then finally increase it to 70
+let bonusPoints = 50
+console.log(bonusPoints)
 
+bonusPoints = bonusPoints * 2 // 100
+console.log(bonusPoints)
 
+bonusPoints = bonusPoints / 4 // 25
+console.log(bonusPoints)
+
+bonusPoints = bonusPoints * 3 - 5 // 70
+console.log(bonusPoints)
