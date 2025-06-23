ac9a165a1215c1298f18a8907766137e10dbab06
Krishna Gehlot <pkrishna.gehlot@gmail.com>
2025-06-24 01:38:15 +0800

    Funtional increment button.

--- learn-javascript-main/1. Build a Passenger Counter App/14. Display the count/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/14. Display the count/index.js
@@ -2,10 +2,12 @@
 
 // change the count-el in the HTML to reflect the new count
 
+let display = document.getElementById("count-el")
 let count = 0
 
 function increment() {
     count = count + 1
+    display.innerText = count
     console.log(count)
 }
 
