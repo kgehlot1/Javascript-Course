2bf6d1b96bef671203dd9794b88c5fa2a9e1e3a0
Krishna Gehlot <pkrishna.gehlot@gmail.com>
2025-06-23 13:45:52 +0800

    Making the increment button.

--- learn-javascript-main/1. Build a Passenger Counter App/08. The onclick event listener/index.js
+++ learn-javascript-main/1. Build a Passenger Counter App/08. The onclick event listener/index.js
@@ -5,7 +5,11 @@
 // console.log(count)
 
 // intialize the count as 0
+let count = 0
 // listen for clicks on the increment button
+function increment() {
+    count = count + 1
+}
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
